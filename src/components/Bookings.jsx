109e47ad7e65ed20fef48e48e9e09e90b0a25518
Your Name <your@email.com>
2025-08-30 20:44:42 +0545

    booking selcet india add

--- src/components/Bookings.jsx
+++ src/components/Bookings.jsx
@@ -29,6 +29,7 @@ const Bookings = () => {
             <label for="appointment-service" className="block text-sm font-medium text-gray-700">Travel Destination</label>
             <select id="appointment-service" name="appointment-service" className="mt-1 block w-full px-3 py-2 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm">
                 <option>NEPAL</option>
+                <option>INDIA</option>
                 <option>BHUTAN</option>
                 <option>TIBET</option>
                 <option>Other</option>
@@ -46,7 +47,7 @@ const Bookings = () => {
            <textarea id="appointment-notes" name="appointment-notes" rows="3" className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm" placeholder="Any specific requests or information?"></textarea>
        </div>
        <div>
-           <button type="submit" className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-purple-600 bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+           <button type="submit" className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-purple-600 bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 ">
                Request Booking
            </button>
        </div>
