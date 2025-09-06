933bfaf2168d095ee4895b1483ca5980f9cdb43c
Your Name <your@email.com>
2025-09-06 08:21:07 +0545

    trekking page

--- src/Pages/Destinations/Nepal/Trekkingandhiking/Trekkingandhiking.jsx
+++ src/Pages/Destinations/Nepal/Trekkingandhiking/Trekkingandhiking.jsx
@@ -8,29 +8,48 @@ export const Trekkingandhiking = () => {
     <>
     <Navbar/>
     <h1 className='mt-28 text-center'>Trekking And Hiking</h1>
+<div className='p-20px'>
 
     {/* passing data using props in cards.jsx */}
     <Cards
      title='Langtang Valley Short Trek '
-       image='https://basecampadventure.com/wp-content/uploads/2024/05/Permits-Required-for-Langtang-Trek.jpg'
-       description='Annapurna, a massif in the Himalayas in north-central Nepal, . The region is also home to the Annapurna Sanctuary, a high-altitude basin surrounded by some of the highest peaks in the world, including Annapurna I, which stands at 8,091 meters (26,545 feet).'
-       />
+     image='https://basecampadventure.com/wp-content/uploads/2024/05/Permits-Required-for-Langtang-Trek.jpg'
+     description='Annapurna, a massif in the Himalayas in north-central Nepal, . The region is also home to the Annapurna Sanctuary, a high-altitude basin surrounded by some of the highest peaks in the world, including Annapurna I, which stands at 8,091 meters (26,545 feet).'
+     />
 
       <Cards
        title=' Tsum Valley Short Trek '
-       image='https://www.authenticindiatours.com/app/uploads/2022/06/Ancient-city-in-Kathmandu-Valley-Patan-Nepal.jpg'
+       image='https://www.nepalhightrek.com/wp-content/uploads/2024/07/Tsum-Valley-Trek.jpg'
        description='Annapurna, a massif in the Himalayas in north-central Nepal, . The region is also home to the Annapurna Sanctuary, a high-altitude basin surrounded by some of the highest peaks in the world, including Annapurna I, which stands at 8,091 meters (26,545 feet).'/>
        
         <Cards
-         title=' City Tour '
-       image='https://www.authenticindiatours.com/app/uploads/2022/06/Ancient-city-in-Kathmandu-Valley-Patan-Nepal.jpg'
+         title=' Everest Base Camp Short Trek '
+       image='https://www.trekkingtoeverest.com/wp-content/uploads/2022/06/Old-Everest-Base-Camp-Trek.jpg'
        description='Annapurna, a massif in the Himalayas in north-central Nepal, . The region is also home to the Annapurna Sanctuary, a high-altitude basin surrounded by some of the highest peaks in the world, including Annapurna I, which stands at 8,091 meters (26,545 feet).'/>
           <Cards
+           title=' Annapurna Base camp Trek '
+           image='https://www.adventurehimalayacircuit.com/public/uploads/annapurna-base-camp-trek-1.jpg'
+           description='Annapurna, a massif in the Himalayas in north-central Nepal, . The region is also home to the Annapurna Sanctuary, a high-altitude basin surrounded by some of the highest peaks in the world, including Annapurna I, which stands at 8,091 meters (26,545 feet).'
+          />
+            <Cards
+           title=' Everest View Trek '
+           image='https://himalayaguidenepal.com/wp-content/uploads/2024/01/Where-is-Mount-Everest-Located.jpg'
+       description='Annapurna, a massif in the Himalayas in north-central Nepal, . The region is also home to the Annapurna Sanctuary, a high-altitude basin surrounded by some of the highest peaks in the world, including Annapurna I, which stands at 8,091 meters (26,545 feet).'
+          />  <Cards
+           title=' City Tour '
+           image='https://www.authenticindiatours.com/app/uploads/2022/06/Ancient-city-in-Kathmandu-Valley-Patan-Nepal.jpg'
+       description='Annapurna, a massif in the Himalayas in north-central Nepal, . The region is also home to the Annapurna Sanctuary, a high-altitude basin surrounded by some of the highest peaks in the world, including Annapurna I, which stands at 8,091 meters (26,545 feet).'
+          />  <Cards
            title=' City Tour '
        image='https://www.authenticindiatours.com/app/uploads/2022/06/Ancient-city-in-Kathmandu-Valley-Patan-Nepal.jpg'
        description='Annapurna, a massif in the Himalayas in north-central Nepal, . The region is also home to the Annapurna Sanctuary, a high-altitude basin surrounded by some of the highest peaks in the world, including Annapurna I, which stands at 8,091 meters (26,545 feet).'
+          />  <Cards
+          title=' City Tour '
+          image='https://www.authenticindiatours.com/app/uploads/2022/06/Ancient-city-in-Kathmandu-Valley-Patan-Nepal.jpg'
+          description='Annapurna, a massif in the Himalayas in north-central Nepal, . The region is also home to the Annapurna Sanctuary, a high-altitude basin surrounded by some of the highest peaks in the world, including Annapurna I, which stands at 8,091 meters (26,545 feet).'
           />
-    <div>Trekkingandhiking</div>
+
+          </div>
 
 
     <Footer/>
