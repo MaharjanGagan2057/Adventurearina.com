a0cf14725e4ba56a2106ee02c5e65dd00aba5903
Your Name <your@email.com>
2025-11-17 08:00:11 +0545

    booking button

--- src/components/Bookings.jsx
+++ src/components/Bookings.jsx
@@ -50,7 +50,7 @@ const Bookings = () => {
            <textarea id="appointment-notes" name="appointment-notes" rows="3" className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm" placeholder="Any specific requests or information?"></textarea>
        </div>
        <div>
-           <button type="submit" className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-purple-600 bg-blue-400 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 ">
+           <button type="submit" className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-purple-600 bg-blue-400 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 m-1 ">
                Request Booking
            </button>
        </div>
